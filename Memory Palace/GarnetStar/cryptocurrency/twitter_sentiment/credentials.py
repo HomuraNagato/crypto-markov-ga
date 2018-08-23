49930ea530b796594e404761eb43013b5413a3e4
HomraNagato <homura.nagato@gmail.com>
2018-08-23 19:35:40 -0400

    tutorial on tweepy sentiment analysis

---
+++ Memory Palace/GarnetStar/cryptocurrency/twitter_sentiment/credentials.py
@@ -0,0 +1,27 @@
+# Twitter App access keys for @Nagato
+
+# Consume: App API Key and Secret
+CONSUMER_KEY     = 'qH2GeyptclhKHtuY008E4cBjb'
+CONSUMER_SECRET  = 'RWzejBc0J935LO8N2Odsqb3L6R2YW6Lk6FILzlGDY5zSCjpBHf'
+
+# Access tokens with read and write access level
+ACCESS_TOKEN     = '703298479339257856-bqkN8DY2kxps8BRlQgNt7Ip5fAPUEn2'
+ACCESS_SECRET    = 'h6WsD0OXzuT60PY7Y4B9uCDM59c7cfq7tHtmlZI9GObDm'
+
+#import matplotlib
+#import wx
+#matplotlib.use("WXAgg")
+#matplotlib.use("qtagg")
+#import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    from PyQt5.QtCore import QT_VERSION_STR
+    from PyQt5.Qt import PYQT_VERSION_STR
+    from sip import SIP_VERSION_STR
+    #print("Qt version:", QT_VERSION_STR)
+    #print("SIP version:", SIP_VERSION_STR)
+    #print("PyQt version:", PYQT_VERSION_STR)
+
+    #print(wx.version())
+
+
